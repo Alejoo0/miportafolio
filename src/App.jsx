03db5788034ec3838d1b338d2a63b6acbc582d13
ALejoo0 <damian.cortes@usm.cl>
2024-12-09 23:27:16 -0300

    Footer 1

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,14 @@
 /** 
  *  Components
  */
-import Header from './components/Header';
-import Hero   from './components/Hero';
-import About  from './components/About';
-import Skill  from './components/Skill';
-import Work from './components/Work';
-import Review from './components/Review';
+import Header  from './components/Header';
+import Hero    from './components/Hero';
+import About   from './components/About';
+import Skill   from './components/Skill';
+import Work    from './components/Work';
+import Review  from './components/Review';
 import Contact from './components/Contact';
+import Footer  from './components/Footer';
 
 const App = () => {
     return (
@@ -25,6 +26,7 @@ const App = () => {
                 <Work />
                 <Review />
                 <Contact />
+                <Footer />
             </main>
         </>
     )
---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+/** 
+ *  @copyright 2023 Damian Cortes
+ * license: Apache License 2.0
+ */
+
+/**
+ * 
+ * Node modules
+ */
+
+
+const Footer = () => {
+
+}
+
+export default Footer;
